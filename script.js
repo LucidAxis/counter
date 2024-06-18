366d91a0d2c76de341f77d82cf029e6db7923097
LucidAxis <toinou.tasserie@gmail.com>
2024-06-18 15:56:45 +0200

    Projet fini

--- script.js
+++ script.js
@@ -1,11 +1,31 @@
-function moins() {
+window.addEventListener("DOMContentLoaded", (event) => {
+  var score = document.getElementById("score");
+  var nbrNombre = 0;
+  var txt = document.querySelector("p");
+  console.log("txt", score)
+  var btnmoins = document.getElementById("moins");
+  var btnplus = document.getElementById("plus");
+  var btnreset = document.getElementById("reset")
 
-}
+  btnplus.addEventListener("click", plus);
+  btnmoins.addEventListener("click", moins);
+  btnreset.addEventListener("click", reset);
 
-function plus() {
+  function plus()
+  {
+    nbrNombre++;
+    score.textContent = nbrNombre;
+  }
+  
+  function moins()
+  {
+    nbrNombre--;
+    score.textContent = nbrNombre;
+  }
 
-}
-
-function reset() {
-    
-}
+  function reset()
+  {
+    nbrNombre=0;
+    score.textContent = nbrNombre;
+  }
+})
